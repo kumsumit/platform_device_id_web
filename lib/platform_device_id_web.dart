95520f5c600567ba81f1a8a3639b12aeb0847b45
sumitsharansatsangi <sharansumitkumar@gmail.com>
2025-05-07 22:30:02 +0530

    first commit

---
+++ lib/platform_device_id_web.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+// In order to *not* need this ignore, consider extracting the "web" version
+// of your plugin as a separate package, instead of inlining it in the same
+// package as the core of your plugin.
+// ignore: avoid_web_libraries_in_flutter
+import 'package:web/web.dart' as web show window;
+
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:platform_device_id_platform_interface/platform_device_id_platform_interface.dart';
+
+/// A web implementation of the PlatformDeviceIdWeb plugin.
+class PlatformDeviceIdWebPlugin extends PlatformDeviceIdPlatform {
+  static void registerWith(Registrar registrar) {
+    PlatformDeviceIdPlatform.instance = PlatformDeviceIdWebPlugin();
+  }
+
+  /// Returns a [String?] containing the ua of the web.
+  @override
+  Future<String?> getDeviceId() async {
+    String? version = web.window.navigator.userAgent;
+    return Future.value(version);
+  }
+}
